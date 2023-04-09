d92d4a10c3d8b662377ee79fa284fab07075e3c2
system5081 <nkrexkai@gmail.com>
2023-04-10 07:51:29 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+//https://mui.com/material-ui/customization/color/
+import { createTheme } from '@mui/material/styles';
+import { lightBlue } from '@mui/material/colors';
+import {red} from '@mui/material/colors';
+//https://mui.com/material-ui/customization/theming/#theme-provider
+//ただしimportに関する記述なし
+import { ThemeProvider } from '@mui/material/styles';
+
+const theme=createTheme({
+  palette :{
+    primary: lightBlue,
+    secondary: red,
+  },
+  typography:{
+    //App.css内でgooglefontから＠importしてCSS rules to specify familiesの記述をここに書く
+    fontFamily: "'M PLUS 1p', sans-serif",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは</h1>
+    </ThemeProvider>
   );
 }
 
